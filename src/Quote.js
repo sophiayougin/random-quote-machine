45ba00a1285f3fac62661f41c051291215e153fd
Sophia Yougin <sophiya273@gmail.com>
2020-11-12 12:33:14 +0530

    used Array.find instead of Array.filter.Fixes #2

--- src/Quote.js
+++ src/Quote.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import data from './data';
  class Quote extends React.Component{
-   constructor(props){
-     super(props);
-   }
    render(){
        const num = this.props.number;
-       const quotes = data.filter(item => item.quoteId === num);
+       const quotes = data.find(item => item.quoteId === num);
        let inputStyle = {
-        color: quotes[0].color
+        color: quotes.color
       };
      return(
        <div>
-        <h3 className = 'quote-text' style={inputStyle}>"{quotes[0].quote}"</h3>
-     <h4 className = 'author' style={inputStyle}>-{quotes[0].author}</h4>
+        <h3 className = 'quote-text' style={inputStyle}>"{quotes.quote}"</h3>
+     <h4 className = 'author' style={inputStyle}>-{quotes.author}</h4>
        </div>
      );
    }
